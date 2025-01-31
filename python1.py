1c8eefceb12982893b26c9593dbcda8ff9e720e5
Yaniv Naor <Yaniv3109@gmail.com>
2025-01-31 14:34:18 +0200

    Python file1

---
+++ python1.py
@@ -0,0 +1,47 @@
+
+# for i in range(1,50):
+#     print("*"*i)
+
+
+# user_input = input("enter your fav dish: ")
+# c = 0
+
+# for n in user_input:
+#     if n == ' ':
+#         c+=1
+   
+# print(c)
+
+
+
+# ב-python יש אפשרות של else גם ב-for, בדומה ל-if.
+
+# correct_pin = '0140'
+
+# for i in range(3):
+#     user_pin = input("enter pin: ")
+#     if user_pin == correct_pin:
+#         print("welocme home")
+#         break
+# else:
+#     print("wrong pin")
+
+
+# תרוץ ללא תנאי, תצא על פי תנאי שיינתן בהמשך. אם לא יינתן תנאי זה יהיה אינסופי
+# # while True
+# while True:
+#     password = input("enter your password: ")
+#     if len(password) >= 10:
+#         print("right")
+#         break
+#     else:
+#         print("wrong")
+
+
+# list = ["hello", "python", "world", "text", "code"]
+
+# for word in list:
+#     if len(word)<5:
+#         break
+#     print(word.upper())
+
